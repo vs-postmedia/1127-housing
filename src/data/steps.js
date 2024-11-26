2dac01db161c5169e849dac4786eb0197c3f58f7
Nathan Griffiths <ngriffiths@postmedia.com>
2024-11-26 13:57:51 -0800

    initial update

--- src/data/steps.js
+++ src/data/steps.js
@@ -1,6 +1,6 @@
 export default [
     '#1: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. <span class="scroll-more">Scroll for more...</span>',
-    '#2: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-    '#3: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod <span class="blue-01">tempor incididunt</span> ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud <span class="green-01">exercitation ullamco laboris</span> nisi ut aliquip ex ea commodo consequat.',
-    '#4: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+    'Nine per cent of Metro households moved at least twice from 2020-2023. These households often preferred a diverse mix of housing.',
+    '20% of Metro households moved once from 2020-2023. Nearly one in 10 of them preferred <span class="grey-03">detached homes</span>',
+    '71% of Metro households didn’t move from 2020-2023. One-third of them preferred <span class="grey-03">detached homes</span>.'
 ];
--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/flourish-scrollytelling-template/',
+  base: '/1127-housing/',
   // base: process.env.MODE === 'production' ? '/09xx-tfwp-story/' : '/',
   plugins: [svelte()],
   resolve: {
