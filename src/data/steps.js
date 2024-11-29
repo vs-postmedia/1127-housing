79d6c38536b99af57ce9b34491cd87552a07e3ee
Nathan Griffiths <ngriffiths@postmedia.com>
2024-11-28 20:08:07 -0800

    copy edit

--- src/data/steps.js
+++ src/data/steps.js
@@ -1,6 +1,6 @@
 export default [
     'A new study by Metro Vancouver has found that 43 per cent of Metro residents still prefer detached, single family homes, though details like income or how often someone moves can have a big impact. <span class="scroll-more">Scroll for more...</span>',
     'Nine per cent of Metro households moved at least twice from 2020-2023. These households often preferred a diverse mix of housing.',
-    '20% of Metro households moved once from 2020-2023. They had a slight preference for <span class="green-01">attached</span> and <span class="grey">detached</span> homes.',
-    '71% of Metro households didn’t move from 2020-2023. One-third of them preferred <span class="grey">detached homes</span>.'
+    '20% of Metro households moved once from 2000-2023. They had a slight preference for <span class="green-01">attached</span> and <span class="grey">detached</span> homes.',
+    '71% of Metro households didn’t move from 2000-2023. One-third of them preferred <span class="grey">detached homes</span>.'
 ];
